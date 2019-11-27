b5855837c3c9082d5844b25971fd22aab9bce8b8
yangqiankun1024 <qiankunyang1024@gmail.com>
2019-11-27 12:32:27 +0800

    feat：对分布式锁进行多线程环境测试，Jmeter，200并发数

--- src/main/java/com/yuntai/shedlock/demo/controller/DistributedLockController.java
+++ src/main/java/com/yuntai/shedlock/demo/controller/DistributedLockController.java
@@ -2,9 +2,12 @@ package com.yuntai.shedlock.demo.controller;
 
 import com.yuntai.shedlock.demo.util.DistributedLockUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBucket;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +20,10 @@ import java.util.concurrent.TimeUnit;
 public class DistributedLockController {
 
     @Autowired
-    RedissonClient redissonClient;
+    private RedissonClient redissonClient;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @GetMapping("/task")
     public void task(){
@@ -28,7 +34,7 @@ public class DistributedLockController {
         try {
             if (getLock = lock.tryLock(0,5, TimeUnit.SECONDS)){
                 //执行业务逻辑
-                System.out.println("拿到锁干活");
+                doSomething();
 
             }else {
                 log.info("Redisson分布式锁没有获得锁:{},ThreadName:{}","LOCK:1001",Thread.currentThread().getName());
@@ -48,28 +54,32 @@ public class DistributedLockController {
 
     @GetMapping("/testUtil")
     public void testUtil(){
-        log.info("testUtil start");
-        boolean getLock = false;
 
+        DistributedLockUtil.lock("LOCK:1001", TimeUnit.SECONDS,5);
         try {
-            if (getLock = DistributedLockUtil.tryLock("LOCK:1001",TimeUnit.SECONDS,0,5)){
-                //执行业务逻辑
-                System.out.println("拿到锁干活");
-
-            }else {
-                log.info("Redisson分布式锁没有获得锁:{},ThreadName:{}","LOCK:1001",Thread.currentThread().getName());
-            }
-
+            doSomething();
         } catch (Exception e) {
             log.error("Redisson 获取分布式锁异常,异常信息:{}",e);
         }finally {
-            if (!getLock){
-                return;
-            }
             //如果演示的话需要注释该代码;实际应该放开
-            //lock.unlock();
-            //log.info("Redisson分布式锁释放锁:{},ThreadName :{}", KeyConst.REDIS_LOCK_KEY, Thread.currentThread().getName());
+            DistributedLockUtil.unlock("LOCK:1001");
+            log.info("Redisson分布式锁释放锁:{},ThreadName :{}", "LOCK:1001", Thread.currentThread().getName());
         }
+    }
+
+    @GetMapping("/test")
+    public void test(){
+        doSomething();
+    }
+
+    private void doSomething(){
+        RedisOperations<String,String> redisOperations = redisTemplate.opsForValue().getOperations();
+        String res = redisOperations.boundValueOps("lock:flag").get();
+        Integer flag = Integer.parseInt(res);
+        flag = flag - 1;
+
+        redisOperations.boundValueOps("lock:flag").set(flag.toString());
+        log.info("do something>>>>>>>>>>>>>{}",redisOperations.boundValueOps("lock:flag").get());
     }
 
 }
