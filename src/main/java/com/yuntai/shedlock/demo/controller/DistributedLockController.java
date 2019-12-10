633fca9ac7579cb26121b292038c5ee68ec024a9
yangqiankun1024 <qiankunyang1024@gmail.com>
2019-12-10 11:03:11 +0800

    chore：修改测试方法

--- src/main/java/com/yuntai/shedlock/demo/controller/DistributedLockController.java
+++ src/main/java/com/yuntai/shedlock/demo/controller/DistributedLockController.java
@@ -25,33 +25,9 @@ public class DistributedLockController {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
-    @GetMapping("/task")
-    public void task() {
-        log.info("task start");
-        RLock lock = redissonClient.getLock("LOCK:1001");
-        boolean getLock = false;
-
-        try {
-            if (getLock = lock.tryLock(0, 5, TimeUnit.SECONDS)) {
-                //执行业务逻辑
-                doSomething();
-
-            } else {
-                log.info("Redisson分布式锁没有获得锁:{},ThreadName:{}", "LOCK:1001", Thread.currentThread().getName());
-            }
-
-        } catch (InterruptedException e) {
-            log.error("Redisson 获取分布式锁异常,异常信息:{}", e);
-        } finally {
-            if (!getLock) {
-                return;
-            }
-            //如果演示的话需要注释该代码;实际应该放开
-            //lock.unlock();
-            //log.info("Redisson分布式锁释放锁:{},ThreadName :{}", KeyConst.REDIS_LOCK_KEY, Thread.currentThread().getName());
-        }
-    }
-
+    /**
+     *
+     */
     @GetMapping("/redissonApi")
     public void redissonApi() {
         log.info("task start");
